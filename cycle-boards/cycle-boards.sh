c132acc00aae839d28af76c1931b2f89b4755179
CameronBarrett33 <92814234+CameronBarrett33@users.noreply.github.com>
2021-11-22 12:16:23 -0800

    Update cycle-boards.sh

--- cycle-boards/cycle-boards.sh
+++ cycle-boards/cycle-boards.sh
@@ -2,22 +2,29 @@
 
 if (systemctl -q is-active mlb-led-scoreboard.service)
 then
+    sudo service mlb-led-scoreboard stop
     sudo systemctl disable mlb-led-scoreboard.service
     sudo systemctl enable nfl-led-scoreboard.service
     sudo service nfl-led-scoreboard start
 
 elif (systemctl -q is-active nfl-led-scoreboard.service)
  then
+    sudo service nfl-led-scoreboard stop
     sudo systemctl disable nfl-led-scoreboard.service
     sudo systemctl enable nhl-led-scoreboard.service
     sudo service nhl-led-scoreboard start
 
 elif (systemctl -q is-active nhl-led-scoreboard.service)
   then
+    sudo service nhl-led-scoreboard stop
     sudo systemctl disable nhl-led-scoreboard.service
     sudo systemctl enable mlb-led-scoreboard.service
     sudo service mlb-led-scoreboard start
- else
+else
+    sudo service nhl-led-scoreboard stop
+    sudo service nfl-led-scoreboard stop
+    sudo systemctl disable nfl-led-scoreboard.service
+    sudo systemctl disable nhl-led-scoreboard.service
     sudo systemctl enable mlb-led-scoreboard.service
     sudo service mlb-led-scoreboard start
     
