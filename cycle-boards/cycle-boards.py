6b87bd9006a2d05dddc37ef1f3ad57c842cfb586
CameronBarrett33 <92814234+CameronBarrett33@users.noreply.github.com>
2021-11-22 12:13:11 -0800

    Update cycle-boards.py

--- cycle-boards/cycle-boards.py
+++ cycle-boards/cycle-boards.py
@@ -1,5 +1,6 @@
 from time import sleep
 import RPi.GPIO as GPIO
+import subprocess
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -11,5 +12,5 @@ GPIO.setup(cycle_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while(1):
       if GPIO.input(cycle_button)==0:
-            print "Cycle board"
+            subprocess.call("./cycle-boards.sh")
             sleep(.1)
