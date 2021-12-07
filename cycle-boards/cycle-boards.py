330e1b3eb7fd4c45fc4a5f36c13ec96a264791a8
CameronBarrett33 <92814234+CameronBarrett33@users.noreply.github.com>
2021-12-06 21:53:52 -0800

    Update cycle-boards.py

--- cycle-boards/cycle-boards.py
+++ cycle-boards/cycle-boards.py
@@ -9,7 +9,7 @@ GND_pin = 19
 
 GPIO.setup(GND_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(cycle_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-curren_state = GPIO.input(GND_pin)
+current_state = GPIO.input(GND_pin)
 
 while(1):
       if GPIO.input(GND_pin)!=current_state:
