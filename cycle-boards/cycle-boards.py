8578f6f2f1c1e28734005bc1f4b1e1631f694e8e
CameronBarrett33 <92814234+CameronBarrett33@users.noreply.github.com>
2022-02-07 17:17:54 -0800

    Update cycle-boards.py

--- cycle-boards/cycle-boards.py
+++ cycle-boards/cycle-boards.py
@@ -12,7 +12,6 @@ GPIO.setup(cycle_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 current_state = GPIO.input(GND_pin)
 
 while(1):
-      if GPIO.input(GND_pin)!=current_state:
-            subprocess.call("./cycle-boards.sh")
-            current_state = GPIO.input(GND_pin)
-            sleep(.1)
+      GPIO.wait_for_edge(cycle_button, GPIO.BOTH, bouncetime=200)
+      print("cycle")
+      subprocess.call("./cycle-boards.sh")
