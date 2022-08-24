1596ccd36cabb9e05bde3a4cb01a5f09aef8c45d
CameronBarrett33 <92814234+CameronBarrett33@users.noreply.github.com>
2022-08-23 20:50:52 -0700

    Update connect-to-wifi.sh

--- connect-to-wifi/connect-to-wifi.sh
+++ connect-to-wifi/connect-to-wifi.sh
@@ -3,4 +3,4 @@
 # clear all contents of wpa_supplicant.config
 sudo sh -c 'cat /home/pi/led-scoreboard-installer/connect-to-wifi/clear-wpa_supplicant.conf > /etc/wpa_supplicant/wpa_supplicant.conf'
 
-sudo sh -c 'wpa_passphrase ${1} ${2} >> /etc/wpa_supplicant/wpa_supplicant.conf'
+sudo sh -c 'wpa_passphrase $@ >> /etc/wpa_supplicant/wpa_supplicant.conf'
